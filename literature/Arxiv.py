74f9dbaaf9ebb73a6009906d6361689217c97268
marcus-meyer-maas <marcus.meyer@maastrichtunversity.nl>
2019-11-14 12:58:09 +0100

    added search terms

--- literature/Arxiv.py
+++ literature/Arxiv.py
@@ -10,11 +10,29 @@ import csv
 
 results = []
 searchterms = ['cross-border',
-             'Common Market Law Review',
-             'cross-border merger',
-             'cross-border conversion',
-             'cross-border division',
-             'company law package']
+              'Common Market Law Review',
+              'cross-border merger',
+              'cross-border conversion',
+              'cross-border division',
+              'company law package',
+              'SE-Directive',
+              'SE Directive',
+              'cross-border mobility',
+              'economical impact of company mobility',
+              'Delaware effect',
+              'cross-border merger inflation', 'cross-border merger GDP','cross-border merger CPI',
+               'cross-border merger wages','cross-border merger market campitalisation',
+               'cross-border merger bond yields','cross-border merger transaction costs',
+               
+               'cross-border inflation', 'cross-border GDP','cross-border CPI','cross-border wages',
+               'cross-border market capitalisation','cross-border bond yields','cross-border transaction costs',
+               
+               'merger and acquisition inflation','merger and acquisition GDP','merger and acquisition CPI',
+               'merger and acquisition wages','merger and acquisition market capitalisation',
+               'merger and acquisition bond yields','merger and acquisition transaction costs',
+               
+              'regression analysis',
+              "Tobin's Q"]
 
 for item in searchterms:
     result = arxiv.query(query=item, max_results=100)
